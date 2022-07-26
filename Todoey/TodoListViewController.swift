a785f41b72585ff4e394e5b5c502539eb882c14f
elahe <el.sh192014@gmail.com>
2022-07-26 10:11:16 +0200

    add tableView Delegate and add bar item

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -30,10 +30,32 @@ class TodoListViewController: UITableViewController {
 //MARK: - TableView Delegate Methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print(itemArray[indexPath.row])
+        //print(itemArray[indexPath.row])
+       
+       
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }else
+        {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
         
        tableView.deselectRow(at: indexPath, animated: false)
     }
-
+    
+    //MARK: - Add New Items
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            //what will happen once the user clicks the Add Item Button on our UIAlert
+            print("Success!")
+        }
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
